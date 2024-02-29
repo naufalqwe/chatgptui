d9d3a0f64195ae3314300434d18bd0fa4ec1ea8f
Naufal <52807329+naufalqwe@users.noreply.github.com>
2024-02-29 07:24:04 +0700

    Add files via upload

--- update.go
+++ update.go
@@ -1,61 +1,105 @@
-package chat
+package config
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 
+	"github.com/dwisiswant0/chatgptui/chat"
 	"github.com/dwisiswant0/chatgptui/common"
 	"github.com/dwisiswant0/chatgptui/style"
+	"github.com/dwisiswant0/chatgptui/util"
 )
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var (
-		tiCmd tea.Cmd
-		vpCmd tea.Cmd
-	)
-
-	m.textarea, tiCmd = m.textarea.Update(msg)
-	m.viewport, vpCmd = m.viewport.Update(msg)
-
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "esc":
 			return m, tea.Quit
-		case "enter":
-			val := m.textarea.Value()
-			if val == "" {
-				return m, nil
+		case "ctrl+r":
+			for i := 0; i <= len(m.inputs)-1; i++ {
+				m.inputs[i].Reset()
 			}
 
-			switch val {
-			case "/c", "/clear":
-				m.viewport.SetContent(common.ChatWelcomeMessage)
-				m.messages = []string{}
-			default:
-				m.messages = append(m.messages, fmt.Sprintf("%s %s", style.Sender.Render("👤:"), val))
+			return m, nil
+		case "tab":
+			if m.focusIndex < len(m.inputs) {
+				i := m.focusIndex
+				v := m.configs[i].defaultValue
+				if v != "" {
+					m.inputs[i].SetValue(v)
+				}
+			}
+		case "enter", "up", "down":
+			s := msg.String()
+			if s == "enter" {
+				if m.focusIndex == len(m.inputs) {
+					if err := m.validateInputs(); err != nil {
+						m.err = err
+						return m, nil
+					} else {
+						m.err = nil
+					}
 
-				res, err := m.sendChat(val)
-				if err != nil {
-					m.err = err
+					if err := m.saveConfig(); err != nil {
+						m.err = err
+						return m, nil
+					} else {
+						cfg, err := Load(common.GetConfigPath())
+						if err != nil {
+							m.err = err
+							return m, nil
+						}
+
+						util.RunProgram(chat.New(cfg))
+						m.err = nil
+					}
+
+					return m, tea.Quit
+				}
+
+				if m.inputs[m.focusIndex].Value() == "" {
 					return m, nil
 				}
 
-				m.messages = append(m.messages, fmt.Sprintf(
-					"%s %s",
-					style.Response.Render("🤖:"),
-					lipgloss.NewStyle().Width(78-5).Render(res)),
-				)
-				m.viewport.SetContent(strings.Join(m.messages, "\n"))
-				m.viewport.GotoBottom()
+				if err := m.validateInput(m.focusIndex); err != nil {
+					m.err = err
+					return m, nil
+				} else {
+					m.err = nil
+				}
 			}
 
-			m.textarea.Reset()
+			if s == "up" {
+				m.focusIndex--
+			} else {
+				m.focusIndex++
+			}
+
+			if m.focusIndex > len(m.inputs) {
+				m.focusIndex = 0
+			} else if m.focusIndex < 0 {
+				m.focusIndex = len(m.inputs)
+			}
+
+			cmds := make([]tea.Cmd, len(m.inputs))
+			for i := 0; i <= len(m.inputs)-1; i++ {
+				if i == m.focusIndex {
+					cmds[i] = m.inputs[i].Focus()
+					m.inputs[i].PromptStyle = style.Focused
+					m.inputs[i].TextStyle = style.Focused
+					continue
+				}
+
+				m.inputs[i].Blur()
+				m.inputs[i].PromptStyle = style.Clear
+				m.inputs[i].TextStyle = style.Clear
+			}
+
+			return m, tea.Batch(cmds...)
 		}
 	}
 
-	return m, tea.Batch(tiCmd, vpCmd)
+	cmd := m.updateInputs(msg)
+
+	return m, cmd
 }
